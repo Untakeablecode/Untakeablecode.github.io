2ce3e904291961a05987fcc24c91a62a18e53521
Untakeablecode <untakeablemine@gmail.com>
2016-09-23 16:50:36 -0400

    Update #3.9

--- send.php
+++ send.php
@@ -1,14 +1,13 @@
 <?php
+if($_POST["submit"]) {
+$recipient = "robbscience@gmail.com";
+$subject = 'Email message from your website';
+$name = $_POST ["yourName"];
+$email = $_POST["yourEmail"];
+$message = $_POST["message-me"];
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-
-$to = "robbscience@gmail.com";
-$subject = "New message from your website";
-$body = "This is an automated message. Do not reply."
-
-mail ($to,$subject,$body, "From: $name <$email>");
-echo "Your Message has been sent";
-
+$mailBody="Name: $name\nEmail: $email\n\n$message";
+mail($recipient, $subject, $mailBody, "From: $name <$email>");
+$thankYou="<p>I will get back to you soon.</p>";
+}
 ?>
