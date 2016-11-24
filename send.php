f0b842076670acc604f82bc105abf6e6b0db23f5
Untakeablecode <untakeablemine@gmail.com>
2016-11-24 10:23:06 -0500

    UPDATE #9.4

--- send.php
+++ send.php
@@ -5,7 +5,7 @@ $message = $_POST['message'];
 
 $to = "robbscience@gmail.com";
 $subject = "New message from your website";
-$body = "This is an automated message. Do not reply."
+$body = "This is an automated message. Do not reply.";
 
 mail ($to,$subject,$body, "From: $name <$email>");
 echo "Your Message has been sent";
